5a83738bd1e0e5949fd894e809da9bcbeb634c4e
unknown <phungducanhTRUYKICH@gmail.com>
2024-03-22 23:36:10 +0700

    QR update

--- front-end/components/qr_components/QrDetail.jsx
+++ front-end/components/qr_components/QrDetail.jsx
@@ -1,39 +1,49 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, Image } from "react-native";
-import { styles } from "./Qr_detail.style";
-
-
-
+import { Text, View, Image, FlatList } from "react-native";
+import styles from './Qr_detail.style';
 
 export default function QrDetail() {
   const fakeQR = require('../../assets/images/qr1.png');
-  const [qrImage, setQrImage] = useState(fakeQR); 
+  const [qrImage, setQrImage] = useState(fakeQR);
 
   const handlePressQrInitial = () => {
     setQrImage(fakeQR);
   };
 
   const handlePressQrOneTime = () => {
-    setQrImage(fakeQR); 
+    setQrImage(fakeQR);
   };
+
+  // Sample data for FlatList
+  const data = [
+    { id: '1', image: qrImage, label: 'QR initial'},
+    { id: '2', image: qrImage, label: 'QR one time' },
+    // Add more items as needed
+  ];
 
   return (
     <View style={styles.container}>
-    <View style={styles.topBar}>
-      <TouchableOpacity onPress={handlePressQrInitial} style={styles.optionBox}>
-        <Text style={styles.optionText}>qr initial</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handlePressQrOneTime} style={styles.optionBox}>
-        <Text style={styles.optionText}>qr one time</Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.qrCodeContainer}>
-      <Image
-        source={qrImage}
-        style={styles.qrImage}
-        resizeMode="contain" 
-        />
-      </View>
+      <FlatList
+        pagingEnabled 
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        data={data}
+        horizontal={true}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>
+              {item.label}
+            </Text>
+            <Image
+              source={item.image}
+              style={styles.qrImage}
+              resizeMode="contain"
+            />
+          </View>
+        )}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.listContent}
+      />
     </View>
   );
 }
--- front-end/components/qr_components/Qr_detail.style.js
+++ front-end/components/qr_components/Qr_detail.style.js
@@ -1,39 +1,31 @@
 import { StyleSheet } from "react-native";
-export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      marginTop: 50, // Adjust as needed
-    },
-    topBar: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      width: '100%',
-      paddingVertical: 10,
-    },
-    optionBox: {
-      borderWidth: 1,
-      borderColor: '#000', // Change as per your design
-      borderRadius: 5, // Optional for rounded corners
-      padding: 10,
-    },
-    optionText: {
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-    qrCodeContainer: {
-      marginTop: 20,
-      width: 264,
-      height: 437,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderColor: 'white', // Placeholder color for the border
-      borderWidth: 1,
-    },
-    qrImage: {
-      width: '100%',
-      height: '100%',
-    },
-    // ...other styles
-  });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContent: {
+    alignItems: 'center',
+  },
+  item: {
+    width: 400,
+    height: 400,
+    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  qrImage: {
+    width: '100%',
+    flex: 1,
+    aspectRatio: 1,
+  },
+  text: {
+    margin: 5,
+    fontSize: 48,
+    fontWeight: 'bold',
+  }
+});
+
+export default styles;
